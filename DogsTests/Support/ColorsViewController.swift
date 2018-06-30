56b1efa225ab68dc11c0c96e8dfc1ef09546aff9
Alexis Aubry <alexis.aubry@wire.com>
2018-06-30 11:36:48 +0200

    Add instructions to trigger error in class causing build failure

--- DogsTests/Support/ColorsViewController.swift
+++ DogsTests/Support/ColorsViewController.swift
@@ -24,7 +24,22 @@ struct ColorItem {
     let size: CGSize
 }
 
+/*
+ * This is the class definition that we think is creating the compilation error.
+ *
+ * `VerticalColumnCollectionViewController` is imported from the `Dogs` app
+ * module. If you make this class a subclass of `UICollectionViewController`
+ * instead, the compilation error will not occur.
+ *
+ * Comment out line 37 and uncomment line 38 to compile the tests successfully.
+ * Reverse to see the compilation error again.
+ *
+ * See comments at lines 54, 58 and 85 to see which parts of the code you need to
+ * comment out after this.
+ */
+
 class ColorViewController: VerticalColumnCollectionViewController {
+// class ColorViewController: UICollectionViewController {
 
     enum TestEnvironment {
         case phone, tablet
@@ -37,9 +52,11 @@ class ColorViewController: VerticalColumnCollectionViewController {
         self.dataSource = dataSource
         self.testEnvironment = testEnvironment
 
+        // Uncomment these two lines when subclassing VerticalColumnCollectionViewController
         let columnLayout = AdaptiveColumnLayout(compact: 2, regular: 3, large: 4)
         super.init(interItemSpacing: 2, interColumnSpacing: 4, columnLayout: columnLayout)
 
+        // Uncomment these three lines when sublassing UICollectionViewController
         //let layout = UICollectionViewFlowLayout()
         //layout.itemSize = CGSize(width: 100, height: 100)
         //super.init(collectionViewLayout: layout)
@@ -65,6 +82,9 @@ class ColorViewController: VerticalColumnCollectionViewController {
         return cell
     }
 
+    // Uncomment the lines between the two markers when subclassing VerticalColumnCollectionViewController
+    // VerticalColumnCollectionViewController-start
+
     override func collectionView(_ collectionView: UICollectionView, sizeOfItemAt indexPath: IndexPath) -> CGSize {
         return dataSource[indexPath.row].size
     }
@@ -72,5 +92,7 @@ class ColorViewController: VerticalColumnCollectionViewController {
     override var isRegularLayout: Bool {
         return testEnvironment == .tablet
     }
+
+    // VerticalColumnCollectionViewController-end
 
 }
